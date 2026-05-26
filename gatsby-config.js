1d18406173389037e61524c48fc145fcd8329881
Bradley Staples <bradleystaples@fastmail.com>
2026-05-26 09:23:47 -0700

    remove old gatsby file

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: 'https://remember-the-ginger.bradleystaples.com',
-    title: 'Remember The Ginger',
-  },
-  plugins: [
-    'gatsby-plugin-sass'
-  ],
-};
